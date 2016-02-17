53ba6d916c9f7d7f096440ab12d130004fefabbc
bravosy <33588968@qq.com>
2016-02-17 16:21:54 +0800

    assignment a3 done

--- a3.py
+++ a3.py
@@ -28,23 +28,60 @@ def optimalZeroPad(x,fs,f):
 	#calculate 0s needed to be padded.
 	sampleEveryPeriod = float(fs)/f
 	integerCycle = np.ceil(length/sampleEveryPeriod)
-	num0 = int(integerCycle) / float(f) * fs - length
-	y = np.zeros(int(num0)+length)
+	num = int(integerCycle * sampleEveryPeriod) - length
+	y = np.zeros(int(num)+length)
 	y[0:length] = x
-	return num0,y
+	return num,y
 
+def testRealEven(x):
+	N = len(x)
+	dftbuffer = zeroPadding(x,N)
+	X = fft(dftbuffer)
+	return dftbuffer,X
+
+def suppressFreqDFTmodel(x,fs,N):
+	"""
+	suppress frequency less than 70Hz.
+	"""
+	length = len(x)
+	window = np.hamming(length)
+	x = x * window
+	dftbuffer = zeroPadding(x,N)
+	X = fft(dftbuffer)
+	k = int(70 * N / fs)	# bins before k is attenuated
+	X[0:k+1] = 0
+	return X[0:N/2+1]
+
+
+def zpFFTsizeExpt():
+	f  = 110.0
+	fs = 1000.0
+	t = np.arange(0,1,1.0/fs)
+	x = cos(2 * pi * f * t)
+	xseg = x[0:256]
+	w1 = np.hamming(256)
+	w2 = np.hamming(512)
+	X1 = fft(xseg * w1)
+	X2 = fft(x[0:512] * w2)
+	X3 = fft(xseg * w1, 512)
+	mx1 = abs(X1)
+	mx2 = abs(X2)
+	mx3 = abs(X3)
+	fx1 = fs * np.arange(256) / 256
+	fx2 = fs * np.arange(512) / 512
+	plt.xlim(0,150)
+	plt.stem(fx1[0:80],mx1[0:80],'y')
+	plt.stem(fx2[0:80],mx2[0:80],'r')
+	plt.stem(fx2[0:80],mx3[0:80],'b')
+	plt.show()
 
 fs = 10000
 f1 = 250
+f2 = 10
+f3 = 30
+f4 = 1000
 t = np.arange(0,1,1.0/fs)
 
-x = cos(2*pi*f1*t) 
-x = x[0:210]
-n,y = optimalZeroPad(x,fs,f1)
+x = cos(2*pi*f1*t) + cos(2*pi*f2*t) +cos(2*pi*f3*t) + cos(2*pi*f4*t) 
+x = x[0:900]
 
-plt.figure(1)
-plt.subplot(211)
-plt.stem(abs(fft(x)))
-
-plt.subplot(212)
-plt.stem(abs(fft(y)))
